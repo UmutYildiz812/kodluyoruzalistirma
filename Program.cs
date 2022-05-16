a331dddd07bc7fbf58db1cc394e8db53a2e1f2a1
UmutYildiz812 <umut_yildiz730@windowslive.com>
2022-05-16 10:19:53 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write("İsminizi Giriniz: ");
+            string name = Console.ReadLine();
+            Console.Write("Soyadınızı Girin: ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Merhaba " + name + " " + surname + ", ben Umut. Bu projeyi kolum kırık olduğu için tek elle yazdım :)");
+            Console.ReadLine();
+
+        }
+    }
+}
