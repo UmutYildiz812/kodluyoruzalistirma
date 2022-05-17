0a7de328cc677b2b32c1e399661448e5d2954083
UmutYildiz812 <umut_yildiz730@windowslive.com>
2022-05-17 08:15:15 +0300

    if else

---
+++ if else/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            //if else şu şekilde yazılabilir:
+            if (time >= 6 && time < 11)
+                Console.WriteLine("Günaydın!");
+            else if (time < 17)
+                Console.WriteLine("İyi Günler!");
+            else
+                Console.WriteLine("İyi akşamlar");
+
+            //şu şekilde de yazabilirsin:
+            string sonuc = time <= 18 ? "İyi Günler!" : "İyi Geceler!";
+            Console.WriteLine(sonuc);
+
+            // üsttekini biraz uzattık:
+            sonuc = time >= 6 && time <= 11 ? "Günaydın" : time <= 18 ? "İyi Günler" : "İyi Geceler";
+            Console.WriteLine(sonuc);
+
+            Console.ReadLine();
+
+        }
+    }
+}
